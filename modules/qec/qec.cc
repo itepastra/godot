126f2e498cf52c4e34ea96748bd7d34bd348379d
Noa Aarts <noa@voorwaarts.nl>
2025-09-27 12:34:07 +0200

    add gates and init functions

--- modules/qec/qec.cc
+++ modules/qec/qec.cc
@@ -1,23 +1,153 @@
 #include "qec.h"
-
-void Qec::add(int p_value) {
-	count += p_value;
-}
-
-void Qec::reset() {
-	count = 0;
-}
-
-int Qec::get_total() const {
-	return count;
-}
+#include <vector>
 
 void Qec::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("add", "value"), &Qec::add);
-	ClassDB::bind_method(D_METHOD("reset"), &Qec::reset);
-	ClassDB::bind_method(D_METHOD("get_total"), &Qec::get_total);
+	ClassDB::bind_method(D_METHOD("init", "qubit_amount"), &Qec::init);
+
+	ClassDB::bind_method(D_METHOD("cnot", "control", "target"), &Qec::cnot);
+	ClassDB::bind_method(D_METHOD("cphase", "control", "target"), &Qec::cphase);
+	ClassDB::bind_method(D_METHOD("hadamard", "qubit"), &Qec::hadamard);
+	ClassDB::bind_method(D_METHOD("phase", "qubit"), &Qec::phase);
+	ClassDB::bind_method(D_METHOD("phase_dag", "qubit"), &Qec::phase_dag);
+	ClassDB::bind_method(D_METHOD("xgate", "qubit"), &Qec::xgate);
+	ClassDB::bind_method(D_METHOD("ygate", "qubit"), &Qec::ygate);
+	ClassDB::bind_method(D_METHOD("zgate", "qubit"), &Qec::zgate);
+}
+
+void Qec::cnot(size_t control, size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	size_t control_block = control >> BLOCK_BITS;
+	size_t target_block = target >> BLOCK_BITS;
+	uint64_t control_inner = powers[control & 63];
+	uint64_t target_inner = powers[target & 63];
+
+	for (size_t i = 0; i < 2 * this->n; i++) {
+		// Flip the x stabilizer on the target, if the control contains 1
+		if (this->x_stabilizers[i][control_block] & control_inner) {
+			this->x_stabilizers[i][target_block] ^= target_inner;
+		}
+		// Flip the z stabilizer on the control, if the target contains +
+		if (this->z_stabilizers[i][target_block] & target_inner) {
+			this->z_stabilizers[i][control_block] ^= control_inner;
+		}
+		// do the phase kickback when all the stabilizers are 1
+		if ((this->x_stabilizers[i][control_block] & control_inner) &&
+				(this->z_stabilizers[i][target_block] & target_inner) &&
+				(this->x_stabilizers[i][target_block] & target_inner) &&
+				(this->z_stabilizers[i][control_block] & control_inner)) {
+			// TODO: check if doing `phase ^= 0b10` is equivalent and faster
+			this->phases[i] = (this->phases[i] + 2) % 4;
+		}
+		// do the phase kickback in another case TODO: figure out what case this represents
+		if ((this->x_stabilizers[i][control_block] & control_inner) &&
+				(this->z_stabilizers[i][target_block] & target_inner) &&
+				!(this->x_stabilizers[i][target_block] & target_inner) &&
+				!(this->z_stabilizers[i][control_block] & control_inner)) {
+			this->phases[i] = (this->phases[i] + 2) % 4;
+		}
+	}
+}
+void Qec::cphase(size_t control, size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	this->hadamard(target);
+	this->cnot(control, target);
+	this->hadamard(target);
+} // CZ = H_target CNOT H_target
+void Qec::hadamard(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	size_t block = target >> BLOCK_BITS;
+	uint64_t inner = powers[target & 63];
+	uint64_t tmp;
+
+	for (size_t i = 0; i < 2 * this->n; i++) {
+		tmp = this->x_stabilizers[i][block];
+		this->x_stabilizers[i][block] ^= (this->x_stabilizers[i][block] ^ this->z_stabilizers[i][block]) & inner;
+		this->z_stabilizers[i][block] ^= (this->z_stabilizers[i][block] ^ tmp) & inner;
+		if ((this->x_stabilizers[i][block] & inner) && (this->z_stabilizers[i][block] & inner)) {
+			this->phases[i] = (this->phases[i] + 2) % 4;
+		}
+	}
+} // H
+void Qec::phase(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	size_t block = target >> BLOCK_BITS;
+	uint64_t inner = powers[target & 63];
+	for (size_t i = 0; i < 2 * this->n; i++) {
+		if ((this->x_stabilizers[i][block] & inner) && (this->z_stabilizers[i][block] & inner)) {
+			this->phases[i] = (this->phases[i] + 2) % 4;
+		}
+		this->z_stabilizers[i][block] ^= this->x_stabilizers[i][block] & inner;
+	}
+} // S
+void Qec::phase_dag(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	this->phase(target);
+	this->phase(target);
+	this->phase(target);
+} // S^+ = S S S
+void Qec::xgate(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	this->hadamard(target);
+	this->phase(target);
+	this->phase(target);
+	this->hadamard(target);
+} // X = H S S H
+void Qec::ygate(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	this->hadamard(target);
+	this->phase(target);
+	this->phase(target);
+	this->hadamard(target);
+	this->phase(target);
+	this->phase(target);
+} // Y = Z X = S S H S S H
+void Qec::zgate(size_t target) {
+	if (!this->initialized) {
+		return;
+	}
+	this->phase(target);
+	this->phase(target);
+} // Z = S S
+
+void Qec::init(size_t qubit_amount) {
+	if (this->initialized) {
+		return;
+	}
+	size_t whole_blocks = (qubit_amount >> BLOCK_BITS) + 1; // shrink by amount of qubits that fit in 64 bits
+	size_t rows = 2 * qubit_amount + 1;
+	this->n = qubit_amount;
+
+	for (size_t i = 0; i < rows; i++) {
+		std::vector<uint64_t> line_x(whole_blocks, 0);
+		std::vector<uint64_t> line_z(whole_blocks, 0);
+		// TODO: what is this for?? gotta either figure it out or ask scott
+		if (i < qubit_amount) {
+			line_x[i >> BLOCK_BITS] = powers[i & 63]; // we create a diagonal
+		} else if (i < rows - 1) {
+			line_z[(i - qubit_amount) >> BLOCK_BITS] = powers[(i - qubit_amount) & 63]; // same with z, but on a different spot
+		}
+		this->x_stabilizers.push_back(line_x);
+		this->z_stabilizers.push_back(line_z);
+		this->phases.push_back(0);
+	}
+
+	this->initialized = true;
 }
 
 Qec::Qec() {
-	count = 0;
+	this->initialized = false;
 }
--- modules/qec/qec.h
+++ modules/qec/qec.h
@@ -1,19 +1,109 @@
 #pragma once
 
 #include "core/object/ref_counted.h"
+#include <cstdint>
+#include <memory>
+#include <vector>
+
+#define BLOCK_BITS 6
 
 class Qec : public RefCounted {
 	GDCLASS(Qec, RefCounted);
 
-	int count;
+	bool initialized;
+	size_t n;
+	std::vector<std::vector<uint64_t>> x_stabilizers; // 2n + 1 by n/4 array (the longs inside are packed bits)
+	std::vector<std::vector<uint64_t>> z_stabilizers; // same but for z stabilizers
+	std::vector<uint_fast8_t> phases;
 
 protected:
+	// godot helper functions
 	static void _bind_methods();
 
-public:
-	void add(int p_value);
-	void reset();
-	int get_total() const;
+	// tableau helper functions
+	void rowcopy(size_t i, size_t k); // set row i equal to row k
+	void rowswap(size_t i, size_t k); // swap row i and row k
+	void rowset(size_t i, size_t b); // set row i to the b-th observable
 
+public:
+	// qubit gates for godot to use
+	void cnot(size_t control, size_t target); // CNOT
+	void hadamard(size_t target); // H
+	void phase(size_t target); // S
+	void phase_dag(size_t target); // S^+ = S S S
+	void xgate(size_t target); // X = H S S H
+	void ygate(size_t target); // Y = X Y = H S S H S S
+	void zgate(size_t target); // Z = S S
+	void cphase(size_t control, size_t target); // CZ = H_target CNOT H_target
+
+	// initialisation
 	Qec();
+	void init(size_t qubit_amount);
+};
+
+const uint64_t powers[1 << BLOCK_BITS] = {
+	1ull << 0,
+	1ull << 1,
+	1ull << 2,
+	1ull << 3,
+	1ull << 4,
+	1ull << 5,
+	1ull << 6,
+	1ull << 7,
+	1ull << 8,
+	1ull << 9,
+	1ull << 10,
+	1ull << 11,
+	1ull << 12,
+	1ull << 13,
+	1ull << 14,
+	1ull << 15,
+	1ull << 16,
+	1ull << 17,
+	1ull << 18,
+	1ull << 19,
+	1ull << 20,
+	1ull << 21,
+	1ull << 22,
+	1ull << 23,
+	1ull << 24,
+	1ull << 25,
+	1ull << 26,
+	1ull << 27,
+	1ull << 28,
+	1ull << 29,
+	1ull << 30,
+	1ull << 31,
+	1ull << 32,
+	1ull << 33,
+	1ull << 34,
+	1ull << 35,
+	1ull << 36,
+	1ull << 37,
+	1ull << 38,
+	1ull << 39,
+	1ull << 40,
+	1ull << 41,
+	1ull << 42,
+	1ull << 43,
+	1ull << 44,
+	1ull << 45,
+	1ull << 46,
+	1ull << 47,
+	1ull << 48,
+	1ull << 49,
+	1ull << 50,
+	1ull << 51,
+	1ull << 52,
+	1ull << 53,
+	1ull << 54,
+	1ull << 55,
+	1ull << 56,
+	1ull << 57,
+	1ull << 58,
+	1ull << 59,
+	1ull << 60,
+	1ull << 61,
+	1ull << 62,
+	1ull << 63,
 };
