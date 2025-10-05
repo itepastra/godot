e0fec750643c025ac934f2de7370f2817bbd6f1e
Noa Aarts <noa@voorwaarts.nl>
2025-10-05 21:43:34 +0200

    modify test to check some hadamard functionality

--- modules/qec/tests/test_qec.h
+++ modules/qec/tests/test_qec.h
@@ -4,34 +4,21 @@
 #include "tests/test_macros.h"
 
 namespace TestQec {
-TEST_CASE("[Modules][Qec] Chained CNOT Gates") {
+TEST_CASE("[Modules][Qec] Hadamard works as expected") {
 	Ref<Qec> q = memnew(Qec);
-	q->init(4);
+	// init to |0>
+	q->init(1);
 
-	// check if all qubits at |0>
 	CHECK(q->get_vop(0) == 10);
-	CHECK(q->get_vop(1) == 10);
-	CHECK(q->get_vop(2) == 10);
-	CHECK(q->get_vop(3) == 10);
 
-	// rotate qubit to |+>
+	// hadamard to |+>
 	q->hadamard(0);
+
 	CHECK(q->get_vop(0) == 0);
 
-	// entangle qubit 0 and 1
-	q->cnot(0, 1);
-	CHECK(q->get_vop(0) == 0);
-	CHECK(q->get_adjacent(0) == PackedInt32Array{ 1 });
-	CHECK(q->get_vop(1) == 10);
-	CHECK(q->get_adjacent(1) == PackedInt32Array{ 0 });
-
-	q->cnot(1, 2);
-
-	q->cnot(2, 3);
+	// hadamard back to |0>
+	q->hadamard(0);
 
 	CHECK(q->get_vop(0) == 10);
-	CHECK(q->get_vop(1) == 10);
-	CHECK(q->get_vop(2) == 10);
-	CHECK(q->get_vop(3) == 10);
 }
 } //namespace TestQec
