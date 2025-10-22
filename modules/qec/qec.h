2a3c8d6c72bd9ce5613102702cf4c015509189c1
Noa Aarts <noa@voorwaarts.nl>
2025-10-22 11:24:53 +0200

    add code to relax a qubit to vop 10 (|0>), first performs measurement

--- modules/qec/qec.h
+++ modules/qec/qec.h
@@ -88,6 +88,8 @@ public:
 	uint8_t my(node_idx node);
 	uint8_t mz(node_idx node);
 
+	void relax(node_idx node);
+
 	PackedByteArray peek_measure_random(PackedInt32Array meas_nodes);
 	uint8_t peek_determinism(node_idx node);
 
